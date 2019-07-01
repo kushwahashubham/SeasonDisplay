28d78417e881a39716f4dcffe47893c630e9bae0
Shubham Kushwaha <Shubham.kushwaha.star@gmail.com>
2019-07-01 16:02:41 +0530

    Initial file structure of season app

---
+++ src/SeasonsDisplay.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+
+const SeasonsDisplay = ()=>{
+    return(
+             <div>
+            Seasons Display
+            </div>
+        );
+}
+
+export default SeasonsDisplay;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React,{Component} from 'react';
+import ReactDom from 'react-dom';
+import SeasonsDisplay from './SeasonsDisplay';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+class App extends Component{
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+    render(){
+        window.navigator.geolocation.getCurrentPosition(
+            (position)=>{console.log(position)},
+            (error)=>{console.log(error)}
+        );
+    
+        return  (
+            <div>
+                <SeasonsDisplay/>
+             </div>
+        );
+
+    }
+    
+}
+
+ReactDom.render(<App/>,document.querySelector('#root'));
