d70e545368f72f103aca961e1d165e97a69b4e91
Shubham Kushwaha <Shubham.kushwaha.star@gmail.com>
2019-07-03 15:02:51 +0530

    some basic work have been done but no styling is done now.

--- src/SeasonsDisplay.js
+++ src/SeasonsDisplay.js
@@ -1,8 +1,19 @@
 import React from 'react';
 import ReactDom from 'react-dom';
 
-const SeasonsDisplay = (props)=>{
-    return <div>Latitude:{props.lat}</div>
+const getSeason = (lat,mon)=>{
+    if(mon>2&&mon<9){
+        if(lat>0)return 'summer';
+        else return 'winter';
+    }else if(lat>0) return 'winter';
+        else return 'summer';
+
 }
 
-export default SeasonsDisplay;
+const SeasonsDisplay = (props)=>{
+    const season = getSeason(props.lat,new Date().getMonth());
+    const text = season === 'winter'? "Burr...it's chilly": "Let's hit the beach"
+    return <div>{text}</div>
+}
+
+export default SeasonsDisplay;  
