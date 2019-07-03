0d5c150e8ff67ab2281b98b36c66b4a74406c955
Shubham Kushwaha <Shubham.kushwaha.star@gmail.com>
2019-07-03 10:32:05 +0530

    this commit having some basic add geolocation related funcnalitiy of index.js

--- src/index.js
+++ src/index.js
@@ -2,19 +2,47 @@ import React,{Component} from 'react';
 import ReactDom from 'react-dom';
 import SeasonsDisplay from './SeasonsDisplay';
 
-class App extends Component{
+class App extends React.Component{
+
+    state ={
+        lat: null,
+        err:null
+    }
+    /* constructor(props){
+        super(props);
+        this.state = {
+            lat:null,
+            err:null
+        }} */
+
+    conditionalRender = ()=>{
+        if(this.state.err) return <div>Error: {this.state.err}</div>
+       else if(this.state.lat) return <div>Latitude: {this.state.lat}</div>;
+       else return <div>Loading ...</div>;
+    }
+    componentDidMount(){
+        window.navigator.geolocation.getCurrentPosition(
+            (position)=>{
+         this.setState({lat: position.coords.latitude});
+            },
+            (error)=>{
+                this.setState({err: error.message});
+            }
+        );
+    }
 
     render(){
-        window.navigator.geolocation.getCurrentPosition(
-            (position)=>{console.log(position)},
-            (error)=>{console.log(error)}
-        );
+       return(
+           <div>
+                {this.conditionalRender()}
+           </div>
+       );
     
-        return  (
+       /*  return  (
             <div>
                 <SeasonsDisplay/>
              </div>
-        );
+        ); */
 
     }
     
